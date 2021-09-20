020fd61f9699157c6c5c687e49924e782aef772c
YoungLocal <surzo18@gmail.com>
2021-09-20 09:19:46 +0200

    First lesson

---
+++ 1.Hodina zdrojáky/src/com/company/Book.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Book {
+
+    /*
+    Todo: attributes
+     */
+
+    public Book(String name, Author author, double price ){
+        //Todo:: Constructor
+        System.out.println("Ahoj svet");
+    }
+
+    /*
+    Todo: other methods
+     */
+}
